8f9ee00bc4de7f9340ee381aedff964691ae19ff
Andoni Angulo <andoni.angulo@zylk.com>
2022-05-31 18:15:10 +0200

    add parameters and formatting

--- mlbuffet_pipeline.py
+++ mlbuffet_pipeline.py
@@ -1,34 +1,35 @@
-import kfp
-import kfp.components as comp
-import glob
+# import kfp
+# import kfp.components as comp
 import zipfile
-import urllib.request
+
 import requests
 
+# Constants and parameters
+remote_zip = 'https://github.com/AlfonsoGonz/kubeflow-mlbuffet-pipeline/raw/main/files/files.zip'
+tag = 'lstm'
+model = 'LSTM2FEATURES.pb'
+mlbuffet_url = f'http://192.168.1.112:30313/api/v1/train/{tag}/{model}'
 
-def download_and_unzip(url: str):
 
-    res = requests.get(url)
+def download_and_unzip(file_url):
+    response = requests.get(file_url)
 
     with open('./files.zip', mode='wb') as file:
-
-        file.write(res.content)
+        file.write(response.content)
 
     with zipfile.ZipFile(file='./files.zip', mode='r') as zip_file:
         zip_file.extractall()
 
 
-def start_training():
-    with open('dataset.csv', 'rb') as dataset, open('./train.py', 'rb') as script, open('requirements.txt', 'rb') as requirements:
-
-        response = requests.post('http://192.168.1.112:30313/api/v1/train/lstm/LSTM2FEATURES.pb',
-                                 files={'dataset': dataset.read(), 'requirements': requirements.read(), 'script': script.read()})
-
-        print(response.content)
+def start_training(server_url):
+    with open('dataset.csv', 'rb') as dataset, open('./train.py', 'rb') as script, open('requirements.txt',
+                                                                                        'rb') as requirements:
+        response = requests.post(server_url, files={'dataset': dataset.read(),
+                                                    'requirements': requirements.read(),
+                                                    'script': script.read()})
+    print(response.content)
 
 
 if __name__ == '__main__':
-    download_and_unzip(
-        url='https://github.com/AlfonsoGonz/kubeflow-mlbuffet-pipeline/raw/main/files/files.zip')
-
-    start_training()
+    download_and_unzip(file_url=remote_zip)
+    start_training(server_url=mlbuffet_url)
